3646d6fe1f3cc8919dea180da198390574d88c98
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-09-26 20:42:07 +0600

    importent link done

--- routes/backend.php
+++ routes/backend.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\settings\AboutController;
 use App\Http\Controllers\settings\GeneralSettingController;
 use App\Http\Controllers\settings\SocialLinkController;
 use App\Http\Controllers\settings\SliderController;
+use App\Http\Controllers\settings\ImportantLinkController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('general-settings',GeneralSettingController::class);
 Route::resource('about-us',AboutController::class);
 Route::resource('social-link',SocialLinkController::class);
 Route::resource('slides',SliderController::class);
+Route::resource('important-links',ImportantLinkController::class);
