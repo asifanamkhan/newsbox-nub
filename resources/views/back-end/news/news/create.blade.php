33a4fef3e2db9cc0218d1255e456349f3abe2f8d
unknown <somuddro249@gmail.com>
2023-10-02 22:19:03 +0600

    news

--- resources/views/back-end/news/news/create.blade.php
+++ resources/views/back-end/news/news/create.blade.php
@@ -1,5 +1,6 @@
 @extends('back-end.layouts.master')
 @section('content-header')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/min/dropzone.min.css">
     <h1 style="font-family: 'Arial Narrow';">
         News Create
     </h1>
@@ -9,6 +10,14 @@
     </ol>
 @endsection
 @section('content')
+    <style>
+        .tox-notifications-container{
+            display: none; !important;
+        }
+        .tox .tox-statusbar__text-container {
+            display: none;
+        }
+    </style>
     <div class="box">
         <div class="box-body">
             @if ($errors->any())
@@ -34,18 +43,46 @@
                         <label for="">Title</label><span style="font-weight: bold; color: red"> *</span>
                         <input type="text" name="title" class="form-control">
                     </div>
-                    <div class="form-group col-md-12">
-                        <label for="">Type</label><span style="font-weight: bold; color: red"> *</span>
-                        <select name="" id="">
-                            <option value="featured">Featured</option>
-                            <option value="latest">Latest</option>
-                            <option value="latest">Trending</option>
-                        </select>
+                    <div class="col-md-7">
+                        <div class="">
+                            <label for="">Description</label><span style="font-weight: bold; color: red"> *</span>
+                            <textarea name="about" id="" class="form-control tinymce-editor" cols="30" rows="10"></textarea>
+                        </div>
                     </div>
-                    <div class="form-group col-md-12">
+                    <div class="col-md-5">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label for="">Date</label><span style="font-weight: bold; color: red"> *</span>
+                                <input type="text" class="form-control  datepicker">
+                            </div>
+                            <div class="col-md-12">
+                                <label for="">Type</label><span style="font-weight: bold; color: red"> *</span>
+                                <select name="" id="" class="form-control select2">
+
+                                </select>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="">Category</label><span style="font-weight: bold; color: red"> *</span>
+                                <select name="" id="" class="form-control select2">
+                                    <option value="">Select</option>
+                                    @foreach($categories as $category)
+                                        <option value="{{$category->id}}">{{$category->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="">Image</label><span style="font-weight: bold; color: red"> *</span>
+                                <div id="dropzoneForm" class="dropzone">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="form-group col-md-12" style="margin-top: 8px">
                         <button class="btn btn-primary">Save</button>
                     </div>
                 </form>
+
             </div>
 
         </div>
@@ -53,11 +90,49 @@
 
 @endsection
 @section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/dropzone.js"></script>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
         $(document).ready(function () {
             $("#side-news").addClass('active');
             $("#side-News").addClass('active');
             $("#side-News").addClass('active-sidebar');
         });
+        tinymce.init({
+            selector: '.tinymce-editor',
+            height: 300,
+        });
+    </script>
+    <script type="text/javascript">
+
+        Dropzone.options.dropzoneForm = {
+            url: "no-path",
+            autoProcessQueue: false,
+            acceptedFiles: ".png,.jpg,.gif,.bmp,.jpeg",
+            addRemoveLinks: true,
+            parallelUploads: 1,
+            maxFilesize: 5,
+            maxFiles: 1,
+            init: function () {
+                this.on("addedfile", (file) => {
+                    if (this.files[1] != null) {
+                        this.removeFile(this.files[0]);
+                    } else {
+                        var reader = new FileReader();
+                        reader.readAsDataURL(file);
+                        reader.onload = event => {
+                            $('#image').val(event.target.result)
+                        }
+                    }
+                });
+
+                this.on('removedfile', function (file) {
+                    $('#image').val(' ');
+                });
+
+            },
+
+        };
+
     </script>
 @endsection
